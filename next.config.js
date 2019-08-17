ab036fef7fb315df91f0151c25aadc8f5d158b20
John Tomanelli II <jptomanelli@gmail.com>
2019-08-16 21:00:43 -0400

    No repo name -_-

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
   cssModules: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://rkcorso.github.io' : ''
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : ''
 });
