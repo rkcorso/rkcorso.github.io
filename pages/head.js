8018cc773cdb80c03a11d2843d20cae424fab962
John Tomanelli II <jptomanelli@gmail.com>
2019-08-16 20:51:11 -0400

    remove unused file

--- pages/head.js
+++
@@ -1,7 +0,0 @@
-import Head from 'next/head';
-
-function Head() {
-    return <link href="https://fonts.googleapis.com/css?family=Lora&display=swap" rel="stylesheet"></link>
-}
-
-export default Head;
