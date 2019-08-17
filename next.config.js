cf3cfb15584adde657211d19ae613e70492ae73a
John Tomanelli II <jptomanelli@gmail.com>
2019-08-16 21:06:03 -0400

    last try

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
-  cssModules: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : ''
+  cssModules: true
 });
