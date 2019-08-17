c027f667a217f271a1489048e7259b245f408bd4
John Tomanelli II <jptomanelli@gmail.com>
2019-08-16 20:57:44 -0400

    add correct asset prefix

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
   cssModules: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/{reponame}' : ''
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://rkcorso.github.io' : ''
 });
